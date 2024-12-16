9beac8345a1ab97b803f5db3f2ac61ac2d52f233
aryan2729 <aryancyber282h@gmail.com>
2024-12-16 18:04:51 +0530

    Axios vs Fetch

---
+++ week 5/offline/index2.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+//🔥Fetch vs 🚀 Axios( better )
+
+
+ //fetch
+async function main(){                             
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const json = await response.json();
+    console.log(json.id);           // id cuz in this above website id present                            
+};
+
+main();
+
+
+//🔥 Axios - External first add  above 
+
+async function Main() {                         // axios easy 
+    
+    const Response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+
+    console.log(Response.data.id);          // alwasy add .data 🚀 
+}
+
+Main();
+
+
+
+
+
+// async function Main() {                         // axios easy (post request )
+    
+//     const Response = await axios.post("https://jsonplaceholder.typicode.com/todos/1");
+    
+//     console.log(Response.data.id);          // alwasy add .data 🚀 
+// }
+
+// Main();
