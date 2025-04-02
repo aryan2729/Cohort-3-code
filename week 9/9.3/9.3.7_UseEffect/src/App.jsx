afb1dd440746834af9dd07a86823bc78a602f2e1
aryan2729 <aryancyber282h@gmail.com>
2025-04-02 18:42:54 +0530

    UseEffect

---
+++ week 9/9.3/9.3.7_UseEffect/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState , useEffect } from "react";
+
+
+function App(){
+
+  const [count , setCount] = useState(1);
+  const [count2 , setCount2] = useState(1);
+
+
+  function increaseCount(){
+    setCount(currentValue => currentValue + 1);   //🔥 this is same as (count + 1) just -> added arrow function 
+  }
+
+  function decreaseCount(){
+    setCount2(currentValue => currentValue - 1);
+  }
+
+  useEffect(function(){   
+    setInterval(increaseCount , 1000 );  //  every 1 sec 
+    setInterval(decreaseCount , 1000) ;
+  }, [] )                                  //🔥 dependency array | this effect will run on mount(start) , because the array is empty
+
+
+  useEffect(function(){
+    console.log("The count has been updated to " + count2);
+  } , [count2])   //🔥 If you pass count2 in dependency array then this above console show in console evertime count2 changes 
+
+
+
+  return <div>
+    
+    {count}{count2}
+
+  </div>
+}
+
+
+export default App;
