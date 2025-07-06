7d0b5bdd30cf146588d80ec9a50501ef1ef67eb6
aryan2729 <aryancyber282h@gmail.com>
2025-07-06 20:12:28 +0530

    Relationship , Transaction & Join

--- week 17/17.2/dist/index.js
+++ week 17/17.2/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+// pg -> Postgres | npm install pg @types/pg
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,7 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// pg -> Postgres | npm install pg @types/pg
+// Relationships -> In neondb while you defining schema of addresses ->  add  FOREGIN KEY(user_id) REFERENCES users(id) DELETE ON CASCADE | so it's  add the id of user table  into addresses table like realtionship and if user deleted then address of that user also deleted (DELETE ON CASCADE)
 const pg_1 = require("pg");
 const express_1 = __importDefault(require("express"));
 const config_1 = require("./config");
@@ -47,5 +48,28 @@ app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* ()
             message: "Error while signing up"
         });
     }
+}));
+app.get("/metadata", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.query.id; //means write like this /metadata?id=3
+    // JOIN -> bad way
+    const query1 = `SElECT  username, email,id  FROM users WHERE id =$1`;
+    const response1 = yield pgClient.query(query1, [id]);
+    const query2 = `SELECT  * FROM addresses WHERE user_id=$1`; // * means everything 
+    const response2 = yield pgClient.query(query2, [id]);
+    res.json({
+        user: response1.rows[0],
+        addresses: response2.rows, //rows cuz it can be more than 1 address stored by user 
+    });
+}));
+app.get("/better-metadata", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.query.id;
+    // JOIN -> Best way
+    const querySelector = `SELECT users.id, users.username, users.email, addresses.city, addresses.country, addresses.street , addresses.pincode
+        FROM users JOIN addresses ON users.id = addresses.user_id
+        WHERE users.id = $1;`; // y $1 not $(id) prevent from sql injection 
+    const response = yield pgClient.query(querySelector, [id]);
+    res.json({
+        response: response.rows //wanted only rows not other info 
+    });
 }));
 app.listen(3000);
--- week 17/17.2/src/index.ts
+++ week 17/17.2/src/index.ts
@@ -58,4 +58,59 @@ app.post("/signup" , async (req , res)=> {
     }
 })
 
+
+app.get("/metadata", async (req , res )=> {
+
+    const id = req.query.id;    //means write like this /metadata?id=3
+
+    // JOIN -> bad way
+    const query1 = `SElECT  username, email,id  FROM users WHERE id =$1`;
+    const response1 = await pgClient.query(query1 , [id]);
+
+    const query2 = `SELECT  * FROM addresses WHERE user_id=$1`;         // * means everything 
+    const response2 = await pgClient.query(query2 , [id]);
+
+    res.json({
+
+        user :  response1.rows[0],
+        addresses : response2.rows,     //rows cuz it can be more than 1 address stored by user 
+    })
+
+})
+
+app.get("/better-metadata" , async (req , res)=> {              
+
+    const id = req.query.id;
+
+    // JOIN -> Best way
+    const querySelector = `SELECT users.id, users.username, users.email, addresses.city, addresses.country, addresses.street , addresses.pincode
+        FROM users JOIN addresses ON users.id = addresses.user_id
+        WHERE users.id = $1;`       // y $1 not $(id) prevent from sql injection 
+
+    const response = await pgClient.query(querySelector ,[id] );
+
+
+    res.json({
+        response : response.rows            //wanted only rows not other info 
+    })
+})
+
 app.listen(3000);
+
+//Type of join 
+// 1. INNER JOIN
+// Concept: Returns only the rows where there's a match in both the users and addresses tables based on users.id = addresses.user_id.
+// What it gives you: A list of users who have at least one address, paired with all their addresses. Users without any addresses will not appear in the result.
+
+// 2. LEFT JOIN 
+// Concept: Returns all rows from the users (left) table, and the matching rows from the addresses (right) table. If a user has no address, the address columns will be NULL.
+// What it gives you: A list of all users, along with their addresses if they have any. Users without addresses will still be listed, but their address fields will be empty (NULL).
+
+// 3. RIGHT JOIN 
+// Concept: Returns all rows from the addresses (right) table, and the matching rows from the users (left) table. If an address has no corresponding user (which shouldn't happen with a proper foreign key, but conceptually), the user columns will be NULL.
+// What it gives you: A list of all addresses, along with the user details for the user associated with that address. If there were an address not linked to any user (which implies bad data or no foreign key constraint), that address would still appear, but the user details would be NULL.
+
+// 4. FULL JOIN (or FULL OUTER JOIN)
+// Concept: Returns all rows when there is a match in either the users (left) or the addresses (right) table. If a row in one table has no match in the other, the non-matching side will have NULL values.
+// What it gives you: 1) if only users not addresses then it gives users 2) if only addresses then it gives that only 3) if both present then it gives both 
+
