8b2a0c8e04499ce440b4305d203d8443dcd6304f
aryan2729 <aryancyber282h@gmail.com>
2025-07-11 01:21:22 +0530

    Next.js

---
+++ week 18/18.2-Next.js/18.2.1-next.js-basic/app/page.tsx
@@ -0,0 +1,11 @@
+// go ->  /signup url for singup that stored in app folder signup folder in which have page.tsx file content 
+
+// npx create-next-app@latest 
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>Home page</div>
+  );
+}
