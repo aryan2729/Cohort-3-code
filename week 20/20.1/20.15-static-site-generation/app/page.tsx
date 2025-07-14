380c25f67bee318fef0eca7a2f8eced9b610e854
aryan2729 <aryancyber282h@gmail.com>
2025-07-15 02:48:33 +0530

    SSR vs CSR vs SSG and much more

---
+++ week 20/20.1/20.15-static-site-generation/app/page.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Home() {
+  return (
+
+    //this below code is SSG static side generation cuz it not containing any functionality that we need to recieve from client 
+    
+    <div>
+
+      Hello 
+      Hi there 
+
+      <button>Click me </button>
+    </div>
+        
+  );
+}
