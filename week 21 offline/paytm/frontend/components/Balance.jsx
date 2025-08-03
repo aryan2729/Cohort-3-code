c32d6c0b919a94de29fb05496c1a24605a90e2e7
aryan2729 <aryancyber282h@gmail.com>
2025-08-04 05:24:26 +0530

    added some code

---
+++ week 21 offline/paytm/frontend/components/Balance.jsx
@@ -0,0 +1,11 @@
+export const Balane = ({value}) => {
+
+    return <div className="flex">
+        <div> Your balance</div>
+       
+   
+    <div className="font-semibold ml-4 text-lg" >
+        Rs {value}
+    </div>
+    </div>
+} 
