02f3cc4f407c9fbed3449c614b030564ea3cf39e
aryan2729 <aryancyber282h@gmail.com>
2025-08-09 16:22:19 +0530

    packages defined and export common files

--- Week 22/draw-app/apps/http-backend/src/index.ts
+++ Week 22/draw-app/apps/http-backend/src/index.ts
@@ -1,12 +1,29 @@
 import express from "express";      // pnpm add express @types/express
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema , SigninSchema , RoomSchema} from "@repo/common/types"
 
 const app = express();
 
 
 app.post("/signup" , ( req , res)=>{
+
+    const data = CreateUserSchema.safeParse(req.body);
+
+    if(!data.success){
+        res.json({
+            message : "Incorrect inputs"
+        })
+        return ;
+    }
+
+    // db calls
+    res.json({
+        userId : 123,
+    })
+
+
 
 })
 
@@ -15,7 +32,7 @@ app.post("/signin", ( req , res)=> {
     const userId = 1;
     const token = jwt.sign({
         userId
-    }, JWT_SECRET)
+    }, JWT_SECRET as string)
 
     res.json({
         token 
---
+++ Week 22/draw-app/packages/backend-common/src/config.ts
@@ -0,0 +1,2 @@
+export const JWT_SECRET = process.env.JWT_SECRET;   // we need to export this as a single file in http and ws backend so we changed package.json and tsconfig too 
+
